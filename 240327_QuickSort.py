150feec0645ddfbd0849b6d9040517ec37c715b7
UntitledPenguin <qramisw@gmail.com>
2024-03-27 20:30:25 +0000

    Untracked files: 	240327_QuickSort_Penguin.py 	240327_QuickSort_Rev2.py

---
+++ 240327_QuickSort.py
@@ -0,0 +1,37 @@
+import random
+import math
+    
+def QuickSort(lst):
+    if len(lst)<2: return lst
+    if len(lst)==2:
+        if lst[0]>lst[1]:
+            s=lst[0]
+            lst[0]=lst[1]
+            lst[1]=s 
+        return lst
+    else:
+        p=lst[0] #extract pivotal value
+        i=1
+        l=len(lst)
+        j=l-1
+        while i<j:            
+            while i<j and lst[i]<=p: i+=1
+            while j>=i and lst[j]>=p: j-=1
+            if i<j:
+                s=lst[i]
+                lst[i]=lst[j]
+                lst[j]=s
+        x=lst[j]
+        lst[j]=p
+        lst[0]=x
+        if j>0: lst[:j]=QuickSort(lst[:j])
+        if j<l-1:lst[j+1:]=QuickSort(lst[j+1:])
+    return lst
+
+n=int(input())
+arr=[]
+scale=int(input())
+for k in range(n):
+    arr.append(random.randint(0, scale))
+print(arr)
+print(QuickSort(arr))
